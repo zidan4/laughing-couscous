e17f3011d33fe2cdb6491defb66e64851d509872
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-03-30 16:41:25 +0300

    Create con.py

---
+++ con.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+y_train_large = (y_train >= '7')
+y_train_odd = (y_train.astype('int8') % 2 == 1)
+y_multilabel = np.c_[y_train_large, y_train_odd]
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_multilabel)
